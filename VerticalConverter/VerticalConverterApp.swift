4d56cda35e0b40f6c862c2b515889ba341c02914
ShmKnd <s.knife0317@gmail.com>
2026-03-13 15:12:43 +0900

    fix: update About window GitHub/X links to actual URLs

--- VerticalConverter/VerticalConverterApp.swift
+++ VerticalConverter/VerticalConverterApp.swift
@@ -22,9 +22,9 @@ struct VerticalConverterApp: App {
                     let html = """
                     <html><body style="font-family:-apple-system;font-size:11px;text-align:center;color:#888">
                     <p style="margin:4px 0">
-                    <a href="https://github.com/YOURNAME/VerticalConverter">GitHub</a>
+                    <a href="https://github.com/ShmKnd">GitHub</a>
                     &nbsp;·&nbsp;
-                    <a href="https://x.com/YOURHANDLE">X (Twitter)</a>
+                    <a href="https://x.com/SHunO_106">X (Twitter)</a>
                     </p>
                     <p style="margin:8px 0 2px;font-size:10px;color:#aaa">
                     MIT License + Commons Clause &copy; 2026 shoma<br>
